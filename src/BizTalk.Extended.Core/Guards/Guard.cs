f12017e9ffb3111fa7e53601c8f80131a028d732
Tom Kerkhove <kerkhove.tom@gmail.com>
2015-07-10 13:09:26 +0200

    Add basic Guard
    Add basic guard for method paramters, tests are up next.

---
+++ src/BizTalk.Extended.Core/Guards/Guard.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BizTalk.Extended.Core.Guards
+{
+    public static class Guard
+    {
+        [DebuggerStepThrough]
+        public static void NotNull<T>(T value, string paramName) where T : class
+        {
+            if (paramName == null)
+            {
+                throw new ArgumentNullException("paramName");
+            }
+
+            if (value == null)
+            {
+                throw new ArgumentNullException(paramName);
+            }
+        }
+
+        [DebuggerStepThrough]
+        public static void NotNullOrEmpty(string value, string paramName)
+        {
+            Guard.NotNull(paramName, "paramName");
+            Guard.NotNull(value, paramName);
+
+            if (value.Length == 0)
+            {
+                throw new ArgumentException("Value cannot be empty", paramName);
+            }
+        }
+
+        [DebuggerStepThrough]
+        public static void Against(bool condition, string paramName)
+        {
+            Guard.NotNull(paramName, "paramName");
+
+            if (condition)
+            {
+                throw new ArgumentException(paramName);
+            }
+        }
+
+        [DebuggerStepThrough]
+        public static void Against<TException>(bool condition) where TException : Exception, new()
+        {
+            if (condition)
+            {
+                throw new TException();
+            }
+        }
+    }
+}
