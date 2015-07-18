c48aeb661d91260eddedbaee9658ba6902627f0d
Tom Kerkhove <kerkhove.tom@gmail.com>
2015-07-18 19:13:00 +0200

    Add temporary NuSpec
    Add temporary NuSpec files untill we have a custom build script

--- src/BizTalk.Extended.Orchestration.Core/Properties/AssemblyInfo.cs
+++ src/BizTalk.Extended.Orchestration.Core/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BizTalk.Extended.Orchestration.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Extensions that take BizTalk orchestration development to the next level.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Sam Neirinck, Tom Kerkhove, Glenn Colpaert, Jonathan Maes")]
 [assembly: AssemblyProduct("BizTalk.Extended.Orchestration.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
@@ -33,4 +33,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0.0")]
