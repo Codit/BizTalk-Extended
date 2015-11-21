a709a783e8175bece726c31ee44d5185b3038fdd
Tom Kerkhove <kerkhove.tom@gmail.com>
2015-11-21 15:56:23 +0100

    Extract generic AssemblyInfo
    Extract generic AssemblyInfo and link to it in each project

--- src/BizTalk.Extended.Pipelines.Extensions/Properties/AssemblyInfo.cs
+++ src/BizTalk.Extended.Pipelines.Extensions/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -7,31 +6,6 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("BizTalk.Extended.Pipelines.Extensions")]
 [assembly: AssemblyDescription("Extensions that take BizTalk pipeline development to the next level.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Sam Neirinck, Tom Kerkhove, Glenn Colpaert, Jonathan Maes")]
-[assembly: AssemblyProduct("BizTalk.Extended.Pipelines.Extensions")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("adc2f501-f327-48e2-83d9-07b91721280f")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
-[assembly: AssemblyInformationalVersion("0.0.0.0")]
